e3856c32cf3102f6aecaecb053c7e64254687e65
Renzo Berrios Telleria <90786322+renbt30@users.noreply.github.com>
2025-01-14 18:55:19 -0500

    Update database.js

--- database.js
+++ database.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql2.createPool({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DBDATABASE,
-    port: process.env.DBPORT,
+    host: 'monorail.proxy.rlwy.net',
+    user: 'root',
+    password: 'HEnoDMGmkJJOpejrKYQwtijWxKGPcChd',
+    database: 'bd_app_delivery',
+    port: 18946,
 });
